8dd21ac861666a7332565643f8f58bb3cfd431a4
Matqui <matqui@metrocuadrado.com>
2022-02-08 22:49:56 -0500

    ajuste en puerto a 8080 y pom para inicializar app

--- src/main/java/co/edu/escuelaing/sparksimpleexample/Server.java
+++ src/main/java/co/edu/escuelaing/sparksimpleexample/Server.java
@@ -1,15 +1,27 @@
 package co.edu.escuelaing.sparksimpleexample;
 
+
+import java.text.DecimalFormat;
+import java.util.Arrays;
+import java.util.List;
+
 import static spark.Spark.*;
 
 public class Server {
 
     public static void main(String[] args) {
-        port(Integer.valueOf(System.getenv("PORT")));
-
+    	port(getPort());
+        
         get("/hello", (req, res) -> "Hello World");
 
-        get("/hello/:nombre", (req, res) -> "Hello World" + req.params(":nombre"));
+        get("/hello/:nombre", (req, res) -> "Hello World " + req.params(":nombre"));
+    }
+    
+    static int getPort(){
+        if(System.getenv("PORT")!= null){
+            return  Integer.parseInt(System.getenv("PORT"));
+        }
+        return 8080;
     }
 
 }
